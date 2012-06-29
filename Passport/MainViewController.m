4fd4f874d8f371d9fde08d5f21f0577d023c3a6a
Maxim Dobryakov <closer.main@gmail.com>
2012-06-29 23:38:47 +0400

    Add Banner

--- Passport/MainViewController.m
+++ Passport/MainViewController.m
@@ -1,8 +1,15 @@
 #import "MainViewController.h"
 #import "SettingsRepository.h"
 #import "LoginViewController.h"
+#import "GADBannerView.h"
+
+#define ADMOB_TOKEN @"a14fede9eb5ac32"
 
 @interface MainViewController () {
+    UIView *container;
+    CGRect defaultContainerFrame;
+
+    GADBannerView *ctlBanner;
 }
 
 @end
@@ -16,6 +23,12 @@
                                              selector:@selector(applicationDidBecomeActive:)
                                                  name:UIApplicationDidBecomeActiveNotification 
                                                object:nil];
+
+    container = [self findContainerView];
+    defaultContainerFrame = container.frame;
+
+    [self initializeBanner];
+    [self showBanner];
 }
 
 - (void)viewDidUnload {
@@ -35,6 +48,56 @@
         LoginViewController *loginViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"LoginView"];
         [self presentModalViewController:loginViewController animated:NO];
     }
+}
+
+#pragma mark Show Banner
+
+// See http://www.hindsightlabs.com/blog/2010/02/08/adding-a-persistent-ad-banner-to-a-uitabbar/
+
+- (void)initializeBanner {
+    ctlBanner = [[GADBannerView alloc] initWithAdSize:kGADAdSizeBanner];
+    ctlBanner.adUnitID = ADMOB_TOKEN;
+    ctlBanner.rootViewController = self;
+    ctlBanner.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+    self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+
+    GADRequest *request = [GADRequest new];
+#ifdef DEBUG
+    request.testing = YES;
+#endif
+
+    [ctlBanner loadRequest:request];
+}
+
+- (UIView *)findContainerView {
+    // A UITabBarController's view has two subviews: the UITabBar and a container UITransitionView that is
+    // used to hold the child views. Save a reference to the container.
+    for (UIView *view in self.view.subviews) {
+        if (![view isKindOfClass:[UITabBar class]]) {
+            return view;
+        }
+    }
+    return nil;
+}
+
+- (void) showBanner {
+    CGFloat containerHeight = defaultContainerFrame.size.height;
+    CGFloat containerWidth = defaultContainerFrame.size.width;
+
+    CGFloat adBannerHeight = ctlBanner.frame.size.height;
+
+    container.frame = CGRectMake(0.0, 0.0, containerWidth, containerHeight - adBannerHeight);
+    ctlBanner.frame = CGRectMake(0.0, containerHeight - adBannerHeight, containerWidth, adBannerHeight);
+
+    [self.view addSubview:ctlBanner];
+}
+
+- (void) hideBanner {
+    CGFloat containerHeight = defaultContainerFrame.size.height;
+    CGFloat containerWidth = defaultContainerFrame.size.width;
+
+    container.frame = CGRectMake(0.0, 0.0, containerWidth, containerHeight);
+    [ctlBanner removeFromSuperview];
 }
 
 @end
